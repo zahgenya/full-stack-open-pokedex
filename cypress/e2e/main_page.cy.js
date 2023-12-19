77c2077ca782c283c70f9e0162d740a6e9b45e22
zahgenya <zahgenya@gmail.com>
2023-12-19 13:56:55 +0200

    fix

--- cypress/e2e/main_page.cy.js
+++ cypress/e2e/main_page.cy.js
@@ -1,6 +1,6 @@
 describe('Pokedex', () => {
   it('front page can be opened', () => {
-    cy.visit('http://localhost:5000')
+    cy.visit('http://localhost:9050')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
