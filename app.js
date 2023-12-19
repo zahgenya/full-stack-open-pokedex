cb941f45ee6700b00b35ab679978d03e254f75fe
zahgenya <zahgenya@gmail.com>
2023-12-19 15:00:26 +0200

    updating localhost

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 9050 // eslint-disable-line
 
 app.use(express.static('dist'))
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   // eslint-disable-next-line
   console.log(`server started on port ${PORT}`)
 })
--- cypress/e2e/main_page.cy.js
+++ cypress/e2e/main_page.cy.js
@@ -1,6 +1,6 @@
 describe('Pokedex', () => {
   it('front page can be opened', () => {
-    cy.visit('http://localhost:9050')
+    cy.visit('http://0.0.0.0:9050')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
